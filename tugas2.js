1e0b61afe418483d255d98a0bda6ff25a1a655f8
farhan <reyhanreynold11@gmail.com>
2021-11-02 00:48:14 +0700

    tugas 2 js

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var string = 'String';
+var number = 29;
+var benar = false;
+var undef = null;
+var array = ['Farhan','Jago'];
+
+console.log(string);
+console.log(number);
+console.log(benar);
+console.log(undef);
+console.log(array);
